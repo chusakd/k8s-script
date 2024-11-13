f438142459a7fa4df4b8f1f7168c416691c46dfa
chusak hanucharoenkul <chusakd@gmail.com>
2024-11-13 15:27:11 +0700

    Update install_k8s_common.sh

--- install_k8s_common.sh
+++ install_k8s_common.sh
@@ -1,3 +1,5 @@
+
+
 swapoff -a
 sed -i '/swap/d' /etc/fstab
 swapon -s
@@ -18,25 +20,10 @@ net.ipv4.ip_forward                 = 1
 EOF
 
 sysctl --system
-OS=xUbuntu_22.04
-CRIO_VERSION=1.24
-
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /"|sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/$OS/ /"|sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.list
-curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:1.24/xUbuntu_22.04/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_22.04/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
-sudo apt-get update 
-sudo apt-get install -qq -y cri-o cri-o-runc cri-tools
-systemctl daemon-reload
-systemctl enable --now crio
-crictl info
-crictl version
-
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl pgp
+sudo mkdir /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt install -y kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
-kubeadm config images pull
