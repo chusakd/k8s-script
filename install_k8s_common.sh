e641ea34cc3e2f20a5b69e72dc8e020e23a62bba
chusak hanucharoenkul <chusakd@gmail.com>
2024-11-13 15:34:44 +0700

    Update install_k8s_common.sh

--- install_k8s_common.sh
+++ install_k8s_common.sh
@@ -4,7 +4,7 @@ swapoff -a
 sed -i '/swap/d' /etc/fstab
 swapon -s
 
-cat >>/etc/modules-load.d/crio.conf<<EOF
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
@@ -13,6 +13,7 @@ modprobe overlay
 modprobe br_netfilter
 lsmod | grep overlay
 lsmod | grep br_netfilter
+
 cat >>/etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables  = 1
@@ -27,3 +28,9 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt install -y kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
+sudo apt install docker.io
+sudo mkdir /etc/containerd
+sudo sh -c "containerd config default > /etc/containerd/config.toml"
+sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl restart containerd.service
+sudo systemctl restart kubelet.service
