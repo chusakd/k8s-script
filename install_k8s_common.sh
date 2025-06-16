99c64dc1fe2011ec749061884914e7f5669bc69a
chusak hanucharoenkul <chusakd@gmail.com>
2025-06-17 03:55:19 +0700

    Update install_k8s_common.sh

--- install_k8s_common.sh
+++ install_k8s_common.sh
@@ -1,16 +1,28 @@
+sudo apt update
+sudo apt install docker.io -y
+sudo systemctl enable docker
+sudo systemctl status docker
 
 
-swapoff -a
-sed -i '/swap/d' /etc/fstab
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt install kubeadm kubelet kubectl
+sudo apt-mark hold kubeadm kubelet kubectl
+kubeadm version
+
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 swapon -s
 
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
 
-modprobe overlay
-modprobe br_netfilter
+sudo modprobe overlay
+sudo modprobe br_netfilter
 lsmod | grep overlay
 lsmod | grep br_netfilter
 
@@ -20,17 +32,6 @@ net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
-sysctl --system
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl pgp software-properties-common
-sudo mkdir /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt install -y kubelet kubeadm kubectl
-sudo systemctl enable --now kubelet
-sudo apt install docker.io
-sudo mkdir /etc/containerd
-sudo sh -c "containerd config default > /etc/containerd/config.toml"
-sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
-sudo systemctl restart containerd.service
-sudo systemctl restart kubelet.service
+sudo sysctl --system
+
+
